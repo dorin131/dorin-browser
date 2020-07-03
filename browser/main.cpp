a7fc7f8348cb52ef7500e54b588155e43b3d029c
Dorin Fodor <dorin.fodor@gmail.com>
2020-07-04 00:17:12 +0100

    [browser] Used Eval to run JS

--- browser/main.cpp
+++ browser/main.cpp
@@ -1,24 +1,11 @@
 #include <X11/Xlib.h>
 #include <unistd.h>
 #include <iostream>
-#include "../JS/tokenizer.h"
-#include "../JS/parser.h"
-#include "../JS/error.h"
+#include "../JS/eval.h"
 
 std::string eval_js(std::string code)
 {
-    js::Tokenizer t = js::Tokenizer(code);
-    auto p = js::Parser(t);
-    std::shared_ptr<js::Program> program;
-    try {
-        program = p.parse_program();
-    } catch (js::SyntaxError e) {
-        return "Syntax error: " + e.message;
-    }
-    js::Interpreter interpreter;
-    auto result = interpreter.run(program);
-
-    return result.get_value();
+    return js::Eval().run(code).get_value();
 }
 
 void draw_window()
