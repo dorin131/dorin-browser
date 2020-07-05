4ab75e31ba1a9148efab3098f81e0e16cac78212
Dorin Fodor <dorin.fodor@gmail.com>
2020-07-05 17:11:05 +0100

    [GUI] Set window background to white

--- GUI/window.cpp
+++ GUI/window.cpp
@@ -9,9 +9,13 @@ DWindow::DWindow()
 
 void DWindow::draw_window(int width, int height)
 {
+    // Attributes
+    XSetWindowAttributes attr;
+    attr.background_pixel = XWhitePixel(display, 0);
+
     // Create the window
     Window window = XCreateWindow(display, DefaultRootWindow(display), 0, 0, width,
-                height, 0, CopyFromParent, CopyFromParent, CopyFromParent, 0, 0);
+                height, 0, CopyFromParent, CopyFromParent, CopyFromParent, CWBackPixel, &attr);
 
     // Show the window
     XMapWindow(display, window);
